45e5e944d6fd0687ab9a5b880abddd9dfef3c4fa
AtulSingh205 <atulsingh07062004@gmail.com>
2025-07-24 11:03:23 +0530

    update

---
+++ spread.js
@@ -0,0 +1,39 @@
+// --------------------copy-----------------
+// let a =[1,2]
+// let b=[...a]
+// console.log(b)
+
+// --------------------merged---------------
+// let a = [1,2]
+// let b = [3,4]
+// let merged=[...a,...b]
+// console.log(merged)
+
+// --------------------meretwoobj-------------
+// let a ={
+//     name:"atul",
+//     id:27
+// }
+// let b = {
+//     name:"karan",
+//     id:37
+// }
+// let arr=[a]
+// let arr2=[b]
+// let merged=[...arr,...arr2]
+// console.log(merged)
+
+// --------------------updatevalue---------------
+// let a={
+//     name:"atul",
+//     id:27
+// }
+// let arr=[a.id=28]
+// let spared=[...arr]
+// console.log(spared)
+// console.log(a)
+
+// --------------------combine----------------
+let bash=[1,2,3,4]
+let  extened=[...bash,30 ,40]
+console.log(extened)
