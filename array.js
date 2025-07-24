b312b031b6dbee4ffe44ffbb6f275a7063769f27
AtulSingh205 <atulsingh07062004@gmail.com>
2025-07-24 06:45:35 +0530

    update new

--- array.js
+++ array.js
@@ -1,7 +1,44 @@
 //---------------Array-------------
-let arr=["Appel","Banana","mango","kela" ,"gwava"]
-console.log(...arr)
-console.log(arr.length)
-console.log(arr[3])
-console.log(arr.push("Orange"))
-console.log(...arr)
+// let arr=["Appel","Banana","mango","kela" ,"gwava"]
+// console.log(...arr)
+// console.log(arr.length)
+// console.log(arr[3])
+// console.log(arr.push("Orange"))
+// console.log(...arr)
+
+// -------------------Arraymed-----------------
+
+// function reverse(arr){
+//         for(let i= arr.length-1; i>=0;i--){
+//             console.log(arr[i])
+//         }
+// }
+// let a =[1,2,3,4,5]
+// let b=reverse(a)
+
+// -----------------duplicate----------------
+// function duplicate(arr){
+//     for(let i =0;i<=arr.length;i++){
+//            if(arr[i]==arr[i+1]){
+//              console.log(`${arr[i]} this is an duplcate element` )
+//            }
+//            else{
+//             console.log(`${arr[i]}`)
+//            }
+
+//     }
+// }
+// let arr=[1,2,2,3,4,5]
+// let a = duplicate(arr)
+
+// -----------------even---------------
+// function even(arr){
+//       for(let i =0;i<=arr.length;i++){
+//         if(arr[i]%2==0){
+//             console.log("This is an even element in array")
+//             console.log(`${arr[i]}`)
+//         }
+//       }
+// }
+// let arr=[1,2,3,4,5]
+// even(arr)
