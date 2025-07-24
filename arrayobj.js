0d6459ae347cf65a375857ac9c3908020568621e
AtulSingh205 <atulsingh07062004@gmail.com>
2025-07-24 10:05:51 +0530

    addnew

---
+++ arrayobj.js
@@ -0,0 +1,52 @@
+// ----------------------arrayobj-----------------
+// let employe1={
+//     name:"Atul",
+//      salary:80000,
+// }
+// let employe2={
+//     name:"Karan",
+//      salary:80000,
+// }
+// let employe3={
+//     name:"Amit",
+//      salary:80000,
+// }
+// let arr=[employe1,employe2,employe3]
+// console.log(...arr)
+// console.log(`print the employe2 key vale ${arr[1].name}`)
+
+// ---------------------arrayofobjects--------------------!!!!!!!!!!!!!!!
+// let  employe1={
+//      position:"hr"
+// }
+// let  employe2={
+//      position:"intern"
+// }
+// let  employe3={
+//      position:"hr"
+// }
+// let arr =[employe1,employe2,employe3]
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i].position==="intern"){
+//         console.log(`${arr[i]} is an inter employe`)
+//     }
+// }
+
+
+// ---------------id-----------------
+// let employe1 = { id: 101, position: "hr" };
+// let employe2 = { id: 102, position: "intern" };
+// let employe3 = { id: 103, position: "hr" };
+
+// let targetid=102;
+// let arr=[employe1,employe2,employe3]
+// for (const e of arr) {
+//     if(e.id==targetid){
+//         console.log(`${e} target id find`)  // pta nhi obj obj ku a rh hai 
+//     }
+// }
+
+// ------------------------adddemp-----------------
+// const arr=[employ1,employe2,employe3]
+// console.log(arr.push("employe4"))
+// console.log(arr)
