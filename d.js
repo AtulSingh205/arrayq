54ccd2328292329a67c5a57a7fe5552a6fc90e9f
AtulSingh205 <atulsingh07062004@gmail.com>
2025-07-24 10:35:40 +0530

    update

--- d.js
+++ d.js
@@ -1,2 +1,37 @@
-let b = 10;
-console.log(b)
+// --------------------distructing----------------
+// const person={
+//      name: "Atul",
+//      age:20
+// }
+// const{name , age }= person; 
+// console.log(person)
+
+// ---------------------distructing in array------------
+
+// let arr=["red","green","blue"]
+// let[a,b,c]=arr
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+// --------------------paranameter-----------------
+
+// let obj={
+//     name:"atul",
+//     age:20,
+// }
+// function greet({name ,age}){
+//     console.log(name,age)
+// }
+// greet(obj)
+
+// ---------------------rename------------
+// let obj={
+//     id:10,
+//     username:"Atul"
+// }
+// let arrfunc=(({id,username},n)=>{
+//     username=n
+//     console.log(id ,username)
+// })
+// arrfunc(obj,"rahul")
