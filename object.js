a3b4e3fa13aa9ec0119aa4f3c618febbe9456b1d
AtulSingh205 <atulsingh07062004@gmail.com>
2025-07-24 07:13:40 +0530

    new file

---
+++ object.js
@@ -0,0 +1,31 @@
+// --------------object-----------------
+// let student={
+//     name: "atul",
+//     age:20,
+//     clg:"shepa",
+//     grade:6.5,
+// }
+// const {name,age,garade}=student;
+// console.log(student)
+// student.city="Varanasi"
+// console.log(student.city)
+
+//----------------objectmed----------
+//      let student={
+//     name: "atul",
+//     age:20,
+//     clg:"shepa",
+//     grade:6.5,
+// }
+
+// function object(s){
+//    console.log(s)
+// }
+// object(student)
+
+// -----------mergedobj-----------
+let arr=[
+    {name:"Atul",age:20},
+    {name:"Karan",age:20}
+]
+console.log(...arr)
